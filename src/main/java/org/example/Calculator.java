617d2d1c8e5eaf2dcfe636d72e486e7d3f096503
sandrine-bd <sandrine.barrucand@le-campus-numerique.fr>
2025-09-04 14:31:30 +0200

    MAJ CalculatorTest

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -1,19 +1,22 @@
 package org.example;
 
 public class Calculator {
-    int add(int a, int b) {
+    public int add(int a, int b) {
         return a + b;
     }
 
-    int sub(int a, int b) {
+    public int sub(int a, int b) {
         return a - b;
     }
 
-    int mul(int a, int b) {
+    public int mul(int a, int b) {
         return a * b;
     }
 
-    double div(int a, int b) {
-        return a / b;
+    public double div(int a, int b) throws ArithmeticException { // double car peut donner des décimaux
+        if (b == 0) {
+            throw new ArithmeticException("Division by zero");
+        }
+        return (double) a / b;
     }
 }
--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -1,51 +1,126 @@
 package org.example;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class CalculatorTest {
-
-    private final Calculator calculator = new Calculator();
+    private static final Calculator calculator = new Calculator();
 
     @Test
-    public void testAddition() {
-        assertEquals(2, calculator.add(1,1));
+    public void testAdd(){
+        // ARRANGE (GIVEN)
+        Calculator target = new Calculator();
+        // ACT (WHEN)
+        int result = target.add(1, 2);
+        // ASSERT (THEN)
+        Assertions.assertEquals(3, result);
+        // WOW
+        Assertions.assertEquals((new Calculator()).add(1,2), result);
     }
 
     @Test
-    public void testAdditionMaxValue() {
-        assertEquals(Integer.MAX_VALUE, calculator.add(Integer.MAX_VALUE,1));
+    public void test_Substract(){
+        Calculator target = new Calculator();
+        int result = target.sub(9,3);
+        Assertions.assertEquals(6, result);
     }
 
     @Test
-    public void testSubtraction() {
-        assertEquals(2, calculator.sub(4, 2));
+    public void test_Multiply(){
+        Calculator target = new Calculator();
+        int result = target.mul(2,7);
+        Assertions.assertEquals(14, result);
     }
 
     @Test
-    public void testMultiplication() {
-        assertEquals(4, calculator.mul(2, 2));
+    public void test_Add_Max_Int_Value(){
+        Calculator target = new Calculator();
+        Assertions.assertEquals(Integer.MAX_VALUE * -1 - 1, target.add(Integer.MAX_VALUE,1));
     }
 
     @Test
-    public void testMultiplicationMaxValue() {
-        assertEquals(Integer.MAX_VALUE, calculator.mul(Integer.MAX_VALUE,2));
+    public void test_Div(){
+        Calculator target = new Calculator();
+        double result = target.div(9,3);
+        Assertions.assertEquals(3, result);
     }
 
     @Test
-    public void testDivision() {
-        assertEquals(2, calculator.div(4, 2));
+    public void test_Div_ByZero(){
+        Calculator target = new Calculator();
+        try {
+            double result = target.div(9,0);
+            Assertions.fail();
+        }
+        catch (ArithmeticException e)
+        {
+            Assertions.assertEquals("/ by zero", e.getMessage());
+        }
+        catch (Exception ex){
+            Assertions.fail();
+        }
     }
 
     @Test
-    public void testDivisionParZero() {
-        assertThrows(ArithmeticException.class, () -> {
-            calculator.div(4, 0);
+    @DisplayName("Division by O should throw an exception")
+    public void test_Div_ByZero_Assert(){
+        Calculator target = new Calculator();
+        Exception exception = Assertions.assertThrows(ArithmeticException.class, () -> {
+            double result = target.div(9,0);
         });
+
+        Assertions.assertEquals("/ by zero", exception.getMessage());
     }
 
     @Test
-    public void testDivisionFloat() {
-        assertEquals(3.333, calculator.div(10, 3), 0.001);
+    @DisplayName("Division should return a double")
+    public void test_Div_Double() {
+        Calculator target = new Calculator();
+        double result = target.div(10, 3);
+        //System.out.println(result);
+        Assertions.assertEquals(3.33333, result, 0.00001);
+    }
+
+    @ParameterizedTest(name = "Test {0} + {1} = {2}")
+    @DisplayName("Test addition")
+    //@ValueSource(ints = {1,2,3,4})
+    @CsvSource({"1,2,3", "4,5,9", "10000,5,10005"})
+    // https://www.baeldung.com/parameterized-tests-junit-5
+    public void test_add_with_parameters(int a, int b, int result) {
+        Calculator target = new Calculator();
+        Assertions.assertEquals(target.add(a, b), result, 0.00001);
+    }
+
+    // Pourquoi ne pas avoir fait des méthodes statiques ? => Si on veut par exemple avoir un historique des calculs réalisés
+
+    // https://www.baeldung.com/junit-before-beforeclass-beforeeach-beforeall
+    private List<String> list;
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("Before each - init");
+        list = new ArrayList<>(Arrays.asList("test1", "test2"));
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("After each - teardown");
+        list.clear();
+    }
+
+    @BeforeAll
+    public static void beforeAll() {
+        System.out.println("Before All");
+        System.out.println(calculator.add(3,2 ));
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        System.out.println("After All");
     }
 }
